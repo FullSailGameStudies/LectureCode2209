71699c90bccff2feccd40d98466e6ad4952d7362
Garrett Girod <girodg@hotmail.com>
2022-08-29 10:34:10 -0400

    PrintMessage, GetMessage

--- Day01/Day01/Program.cs
+++ Day01/Day01/Program.cs
@@ -6,7 +6,29 @@ namespace Day01
     {
         static void Main(string[] args)
         {
+
+            int n1 = 5, n2 = 2;
+            int sum = Add(n1, n2);
+            sum = Add(7, 3);
+            PrintMessage();
+
+            string message = GetMessage();
+        }
+        static int Add(int num1, int num2)
+        {
+            return num1 + num2;
         }
 
+        static void PrintMessage()
+        {
+            Console.WriteLine("Hello Gotham!");
+        }
+
+        static string GetMessage()
+        {
+            Console.Write("Please enter a message: ");
+            string msg = Console.ReadLine();
+            return msg;
+        }
     }
 }

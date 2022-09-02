4400a0b3c07c57d1f3c4b68edaa9506d396dd8b7
Garrett Girod <girodg@hotmail.com>
2022-09-02 10:33:40 -0400

    PrintGrades

--- Day03/Day03/Program.cs
+++ Day03/Day03/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 
 namespace Day03
 {
@@ -34,13 +35,23 @@ namespace Day03
             Menu["Impossible Burger"] = 8.99F;
             Menu["Impossible Burger"] = 12.99F;//will NOT throw. will overwrite the value
 
+
+            Menu["Chicken Burger"] = 7.99F;
+            Menu["Chicken Nuggets"] = 3.99F;
+
+            Menu["Fries"] = 3.99F;
+            Menu["Sweet Potato Fries"] = 5.99F;
+            Menu["Loaded Fries"] = 6.99F;
+            Menu["Curly Fries"] = 3.99F;
+
         }
 
         public void Print()
         {
-            //Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.BackgroundColor = ConsoleColor.DarkGray;
             Console.Clear();
 
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
             string title = $"--------{Name} Menu---------";
             int x = Console.WindowWidth / 2 - title.Length / 2;
             int y = Console.WindowHeight / 2 - Menu.Count / 2;
@@ -54,8 +65,18 @@ namespace Day03
 
                 string menuItem = item.Key;
                 float price = item.Value;
-                Console.WriteLine($"{price,9:C2} {menuItem}");
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write($"{price,9:C2} ");
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine($"{menuItem}");
             }
+
+            //foreach (var color in Enum.GetValues<ConsoleColor>())
+            //{
+            //    Console.BackgroundColor = color;
+            //    Console.WriteLine($"                   {color}");
+            //}
             Console.ResetColor();
         }
     }
@@ -82,6 +103,29 @@ namespace Day03
 
             pg2["Jon"] = rando.NextDouble() * 100;
             pg2["Devasean"] = rando.NextDouble() * 100;
+            pg2["Charlie"] = rando.NextDouble() * 100;
+
+            PrintGrades(pg2);
+        }
+
+        static void PrintGrades(Dictionary<string, double> course)
+        {
+            Console.Clear();
+            Console.WriteLine("---------GRADES---------");
+            foreach (KeyValuePair<string,double> student in course)
+            {
+                string name = student.Key;
+                double grade = student.Value;
+                Console.ForegroundColor = (grade < 59.5) ? ConsoleColor.Red :
+                                          (grade < 69.5) ? ConsoleColor.DarkYellow :
+                                          (grade < 79.5) ? ConsoleColor.Yellow :
+                                          (grade < 89.5) ? ConsoleColor.Blue :
+                                          ConsoleColor.Green;
+                if (grade < 59.5) Console.BackgroundColor = ConsoleColor.DarkBlue;
+                Console.Write($"{grade,9:N2}  ");
+                Console.ResetColor();
+                Console.WriteLine(name);
+            }
         }
     }
 }

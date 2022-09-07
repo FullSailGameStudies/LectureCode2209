ddb49f76aefaf4fbbbdf12a50293740432198f88
Garrett Girod <girodg@hotmail.com>
2022-09-07 09:38:41 -0400

    writing csv

--- Day04/Day04/Program.cs
+++ Day04/Day04/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 
 namespace Day04
@@ -8,6 +9,20 @@ namespace Day04
     {
         static void Main(string[] args)
         {
+            string fileName = @"C:\temp\2209\heroes.txt";
+            char delimiter = '*';
+            //1. open the file
+            using (StreamWriter sw = new StreamWriter(fileName))
+            {
+                //2. write to the file
+                sw.Write("Batman");
+                sw.Write(delimiter);
+                sw.Write(5);
+                sw.Write(delimiter);
+                sw.Write(13.7);
+                sw.Write(delimiter);
+                sw.Write(true);
+            }//3. Close the file!  sw.Close();
         }
     }
 }

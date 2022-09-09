e67534fa7ac094a7808d160364cf8d47b6af0ea7
Garrett Girod <girodg@hotmail.com>
2022-09-09 11:19:01 -0400

    LinearSearch

--- Day06/Day06/Program.cs
+++ Day06/Day06/Program.cs
@@ -8,6 +8,30 @@ namespace Day06
     {
         static void Main(string[] args)
         {
+            List<int> nums = new List<int>() { 1, 5, 8, 13, 42 };
+            int numberToFind = 42;
+            int indexOf = LinearSearch(nums, numberToFind);
+            if(indexOf == -1)
+                Console.WriteLine($"{numberToFind} was not found!");
+            else
+                Console.WriteLine($"{numberToFind} was found at index {indexOf}");
+        }
+
+        //performance: O(N) - linear where N is the # of items in the list
+        private static int LinearSearch(List<int> nums, int numberToFind)
+        {
+            int index = -1;//-1 means not found
+
+            for (int i = 0; i < nums.Count; i++)
+            {
+                if(numberToFind == nums[i])
+                {
+                    index = i;
+                    break;
+                }
+            }
+
+            return index;
         }
     }
 }

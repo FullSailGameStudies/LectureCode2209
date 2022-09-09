bfff47a6801ff08e41e41de51de4aad850bbcfc3
Garrett Girod <girodg@hotmail.com>
2022-09-09 10:48:43 -0400

    Split

--- Day05/Day05/Program.cs
+++ Day05/Day05/Program.cs
@@ -15,6 +15,19 @@ namespace Day05
                 Console.Write($"{numbers[i]} ");
             }
             Console.WriteLine();
+
+            Split(numbers.ToList());
+
+            //CompareTo
+            //returns these values...
+            // -1 means LESS THAN
+            //  0 means EQUAL TO
+            //  1 means GREATER THAN
+            string s1 = "Aquamans", s2 = "Aquaman";
+            int compareResult = s2.CompareTo(s1);
+            if (compareResult == 0) Console.WriteLine($"{s2} EQUALS {s1}");
+            else if(compareResult > 0) Console.WriteLine($"{s2} GREATER THAN {s1}");
+            else if (compareResult == -1) Console.WriteLine($"{s2} LESS THAN {s1}");
             Console.ReadKey();
 
             for (int i = 0; i < 100; i++)
@@ -28,6 +41,29 @@ namespace Day05
             ulong result = Factorial(5);
             Console.WriteLine($"5! = {result}");
 
+        }
+
+        private static void Split(List<int> m)
+        {
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+            int mid = m.Count / 2;
+            for (int i = 0; i < m.Count; i++)
+            {
+                if (i < mid)
+                    left.Add(m[i]);
+                else
+                    right.Add(m[i]);
+            }
+            Console.WriteLine("------------------LEFT---------------");
+            foreach(int number in left)
+                Console.Write($"{number} ");
+            Console.WriteLine();
+
+            Console.WriteLine("------------------RIGHT---------------");
+            foreach (int number in right)
+                Console.Write($"{number} ");
+            Console.WriteLine();
         }
 
         private static void Swap(int[] numbers, int index1, int index2)

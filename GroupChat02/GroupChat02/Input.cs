50d5d6d79a50ab681829ee8792ea78562c7f18fd
Garrett Girod <girodg@hotmail.com>
2022-09-10 14:42:31 -0400

    Group Chat, week 2

---
+++ GroupChat02/GroupChat02/Input.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GroupChat02
+{
+    public static class Input
+    {
+        public static int ReadInteger(string prompt, int min, int max)
+        {
+            while(true)
+            {
+                Console.Write(prompt);
+                if (int.TryParse(Console.ReadLine(), out int number) && number >= min && number <= max)
+                    return number;//valid number
+                Console.WriteLine("That is not a valid integer.");
+            }
+        }
+
+        public static void ReadString(string prompt, ref string value)
+        {
+            do
+            {
+                Console.Write(prompt);
+                value = Console.ReadLine();
+                if (!string.IsNullOrWhiteSpace(value))
+                    break;
+                Console.WriteLine("That is not a valid string.");
+            } while (true);
+        }
+
+        public static void ReadChoice(string prompt, string[] options, out int selection)
+        {
+            foreach(string menuOption in options)
+                Console.WriteLine(menuOption);
+
+            selection = ReadInteger(prompt, 1, options.Length);
+        }
+    }
+}
---
+++ GroupChat02/GroupChat02/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GroupChat02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] menu = new string[] { "1. Play Game", "2. Load Game", "3. Exit" };
+            int menuSelection;
+            do
+            {
+                Console.Clear();
+                Input.ReadChoice("Choice? ", menu, out menuSelection);
+                switch (menuSelection)
+                {
+                    case 1:
+                        break;
+                    case 2:
+                        string saveGame = string.Empty;
+                        Input.ReadString("Enter save game name: ", ref saveGame);
+                        break;
+                }
+            } while (menuSelection != 3);
+        }
+    }
+}
